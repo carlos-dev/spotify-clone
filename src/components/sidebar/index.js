a2254f7783e298f805b6cb563f57439d4cdd92bc
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2019-12-04 10:32:23 -0300

    sidebar criada

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Container, NewPlaylist } from './styles';
+import { Container, NewPlaylist, Nav } from './styles';
 
 import AddPlaylistIcon from '../../assets/imges/add_playlist.svg'
 
@@ -8,7 +8,55 @@ export default class componentName extends Component {
   render() {
     return (
       <Container>
-        <div />
+        <div>
+          <Nav weightBold>
+            <li>
+              <a href="">Navegar</a>
+            </li>
+            <li>
+              <a href="">Rádio</a>
+            </li>
+          </Nav>
+
+          <Nav>
+            <li>
+              <span>SUA BIBLIOTECA</span>
+            </li>
+            <li>
+              <a href="">Seu Daily Mix</a>
+            </li>
+            <li>
+              <a href="">Tocados recentemente</a>
+            </li>
+            <li>
+              <a href="">Álbuns</a>
+            </li>
+            <li>
+              <a href="">Artistas</a>
+            </li>
+            <li>
+              <a href="">Estações</a>
+            </li>
+            <li>
+              <a href="">Arquivos Locais</a>
+            </li>
+            <li>
+              <a href="">Vídeos</a>
+            </li>
+            <li>
+              <a href="">Podcasts</a>
+            </li>
+          </Nav>
+
+          <Nav>
+            <li>
+              <span>PLAYLISTS</span>
+            </li>
+            <li>
+              <a href="">As Melhores do Rock</a>
+            </li>
+          </Nav>
+        </div>
 
         <NewPlaylist>
           <img src={AddPlaylistIcon} alt="adicionar playlist" />
--- src/components/sidebar/styles.js
+++ src/components/sidebar/styles.js
@@ -4,10 +4,45 @@ export const Container = styled.aside`
   height: 100vh;
   width: 200px;
   background: #121212;
-  color: #3b3b3b;
+  color: #b3b3b3;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+
+  > div {
+    padding: 25px;
+  }
+`;
+
+export const Nav = styled.nav`
+  list-style: none;
+  margin-top: 25px;
+
+  &:first-child {
+    margin-top: 0;
+  }
+
+  li {
+    a {
+      color: inherit;
+      text-decoration: none;
+      font-size: 13px;
+      line-height: 32px;
+      font-weight: ${props => props.weightBold ? 'bold' : 'normal'}
+
+      &:hover {
+        color: #fff;
+      }
+    }
+
+    span {
+      font-size: 11px;
+      text-transform: uppercase;
+      line-height: 22px;
+      letter-spacing: 1.11px;
+      font-weight: 300;
+    }
+  }
 `;
 
 export const NewPlaylist = styled.button`
