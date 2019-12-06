9217ec2a7d21339906236901f7729af1beea4190
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2019-12-06 01:33:24 -0300

    criando as rotas

---
+++ src/pages/browse/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class Browse extends Component {
+  render() {
+    return (
+      <h1>hello browse</h1>
+    )
+  }
+}
