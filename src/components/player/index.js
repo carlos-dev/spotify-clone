a083843f191f0ce6129589384a2f8059510d48ad
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2020-03-09 23:48:36 -0300

    tocando a música

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -1,22 +1,45 @@
-import React, { Component } from 'react';
-import Slider from 'rc-slider';
+import React, { Component } from "react";
+import Slider from "rc-slider";
+import Sound from "react-sound";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import PropTypes from "prop-types";
 
-import { Container, Current,  Volume, Progress, Controls, Time, ProgressSlider } from './styles';
+import {
+  Container,
+  Current,
+  Volume,
+  Progress,
+  Controls,
+  Time,
+  ProgressSlider
+} from "./styles";
 
-import VolumeIcon from '../../assets/imges/volume.svg';
-import ShuffleIcon from '../../assets/imges/shuffle.svg';
-import BackwardIcon from '../../assets/imges/backward.svg';
-import PlayIcon from '../../assets/imges/play.svg';
-import PauseIcon from '../../assets/imges/pause.svg';
-import ForwardIcon from '../../assets/imges/forward.svg';
-import RepeatIcon from '../../assets/imges/repeat.svg';
+import VolumeIcon from "../../assets/imges/volume.svg";
+import ShuffleIcon from "../../assets/imges/shuffle.svg";
+import BackwardIcon from "../../assets/imges/backward.svg";
+import PlayIcon from "../../assets/imges/play.svg";
+import PauseIcon from "../../assets/imges/pause.svg";
+import ForwardIcon from "../../assets/imges/forward.svg";
+import RepeatIcon from "../../assets/imges/repeat.svg";
 
-export default class Player extends Component {
+class Player extends Component {
   render() {
+    const { player } = this.props;
     return (
       <Container>
+        {!!player.currentSong && (
+          <Sound url={player.currentSong.file} playStatus={player.status} />
+        )}
+        {/* <Sound
+          url=""
+        /> */}
+
         <Current>
-          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWpz9c7qrYEluBY8OvRW4oRln_wgyhrvgo5gab9fVoGDCvwQ9t&s' alt="musica" />
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWpz9c7qrYEluBY8OvRW4oRln_wgyhrvgo5gab9fVoGDCvwQ9t&s"
+            alt="musica"
+          />
 
           <div>
             <span>Times like these</span>
@@ -26,22 +49,32 @@ export default class Player extends Component {
 
         <Progress>
           <Controls>
-            <button><img src={ShuffleIcon} alt="Shuffle" /></button>
-            <button><img src={BackwardIcon} alt="Bakckward" /></button>
-            <button><img src={PlayIcon} alt="Play" /></button>
-            <button><img src={ForwardIcon} alt="Forward" /></button>
-            <button><img src={RepeatIcon} alt="Repeat" /></button>
+            <button>
+              <img src={ShuffleIcon} alt="Shuffle" />
+            </button>
+            <button>
+              <img src={BackwardIcon} alt="Bakckward" />
+            </button>
+            <button>
+              <img src={PlayIcon} alt="Play" />
+            </button>
+            <button>
+              <img src={ForwardIcon} alt="Forward" />
+            </button>
+            <button>
+              <img src={RepeatIcon} alt="Repeat" />
+            </button>
           </Controls>
 
           <Time>
             <span>1:37</span>
-              <ProgressSlider>
-                <Slider
-                  railStyle={{ background: '#404040', borderRadius: 10 }}
-                  trackStyle={{background: '#1ed760'}}
-                  handleStyle={{ border: 0 }}
-                />
-              </ProgressSlider>
+            <ProgressSlider>
+              <Slider
+                railStyle={{ background: "#404040", borderRadius: 10 }}
+                trackStyle={{ background: "#1ed760" }}
+                handleStyle={{ border: 0 }}
+              />
+            </ProgressSlider>
             <span>3:40</span>
           </Time>
         </Progress>
@@ -49,9 +82,9 @@ export default class Player extends Component {
         <Volume>
           <img src={VolumeIcon} alt="volume" />
           <Slider
-            railStyle={{ background: '#404040', borderRadius: 10 }}
-            trackStyle={{ background: '#fff' }}
-            handleStyle={{display: 'none'}}
+            railStyle={{ background: "#404040", borderRadius: 10 }}
+            trackStyle={{ background: "#fff" }}
+            handleStyle={{ display: "none" }}
             value={100}
           />
         </Volume>
@@ -59,3 +92,18 @@ export default class Player extends Component {
     );
   }
 }
+
+Player.propTypes = {
+  player: PropTypes.shape({
+    currentSong: PropTypes.shape({
+      file: PropTypes.string
+    }),
+    status: PropTypes.string
+  }).isRequired
+};
+
+const mapStateToProps = state => ({
+  player: state.player
+});
+
+export default connect(mapStateToProps)(Player);
--- src/pages/playlist/index.js
+++ src/pages/playlist/index.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { Creators as PlaylistDetailsActions } from "../../store/ducks/playlistDetails";
+import { Creators as PlayerActions } from "../../store/ducks/player";
 
 import Loading from "../../components/loading";
 
@@ -35,7 +36,8 @@ class Playlist extends Component {
         )
       }),
       loading: PropTypes.bool
-    })
+    }).isRequired,
+    loadSong: PropTypes.func.isRequired
   };
 
   componentDidMount() {
@@ -89,7 +91,10 @@ class Playlist extends Component {
               </tr>
             ) : (
               playlist.songs.map(song => (
-                <tr key={song.id}>
+                <tr
+                  key={song.id}
+                  onDoubleClick={() => this.props.loadSong(song)}
+                >
                   <td>
                     <img src={PlusIcon} alt="Adicionar" />
                   </td>
@@ -124,6 +129,6 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch =>
-  bindActionCreators(PlaylistDetailsActions, dispatch);
+  bindActionCreators({ ...PlaylistDetailsActions, ...PlayerActions }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(Playlist);
